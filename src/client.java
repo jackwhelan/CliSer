6e670ebc4bd54c95a52093646699706c0e0774b4
Jack Whelan <jackw97@LIVE.IE>
2019-05-22 14:19:52 +0100

    Completely overhauled - Now multi-threaded.
    CliSer is now multithreaded and allows for the passing of messages to
    and from the server and client. The client is run with the user's name
    as an argument, this results in all messages sent to the server to be
    prefixed with their name.

--- src/client.java
+++ src/client.java
@@ -1,23 +1,91 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
-public class client
+public class Client
 {
 	public static void main(String[] args)
 	{
+		Socket client = null;
 		try
 		{
-			Socket client = new Socket( "localhost", 5000 );
-			System.out.println("Client is connected to Server");
-			ObjectOutputStream out = new ObjectOutputStream( client.getOutputStream() );
-			ObjectInputStream in = new ObjectInputStream( client.getInputStream() );
-			String message = ( String ) in.readObject();
-			System.out.println("Data from Server: " + message);
-			message = "Hello";
-			out.writeObject(message);
-			client.close();
+			client = new Socket("176.61.21.93", 5000);
+			System.out.println("Connected to server.");
 		}
-		catch (Exception ex)
+		catch(Exception ex)
+		{
+			System.err.println(ex);
+		}
+		
+		try
+		{
+			Thread r = new ReadingThread(client);
+			Thread w = new WritingThread(client, args[0]);
+			w.start();
+			r.start();
+		}
+		catch(Exception ex)
+		{
+			System.err.println(ex);
+		}
+	}
+}
+
+class ReadingThread extends Thread
+{
+	private Socket connection;
+	
+	public ReadingThread(Socket con)
+	{
+		connection = con;
+	}
+	
+	public void run()
+	{
+		try
+		{
+			ObjectInputStream in = new ObjectInputStream (connection.getInputStream());
+			String message;
+			
+			while (true)
+			{
+				message = (String)in.readObject();
+				System.out.println(message);
+			}
+		}
+		catch(Exception ex)
+		{
+			System.err.println(ex);
+		}
+	}
+}
+
+class WritingThread extends Thread
+{
+	private Socket connection;
+	String username;
+	
+	public WritingThread(Socket con, String uname)
+	{
+		connection = con;
+		username = uname;
+	}
+	
+	public void run()
+	{
+		try
+		{
+			ObjectOutputStream out = new ObjectOutputStream(connection.getOutputStream());
+			String message;
+			Scanner sc = new Scanner(System.in);
+			
+			while(true)
+			{
+				message = sc.nextLine();
+				out.writeObject(username + ": " + message);
+			}
+		}
+		catch(Exception ex)
 		{
 			System.err.println(ex);
 		}
--- src/server.java
+++ src/server.java
@@ -1,24 +1,95 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
-public class server
+public class Server
 {
 	public static void main(String[] args)
 	{
+		ServerSocket server = null;
+		
 		try
 		{
-			ServerSocket server = new ServerSocket( 5000, 10 );
-			System.out.println("Server is running on port 5000");
-			Socket connection = server.accept();
-			ObjectOutputStream out = new ObjectOutputStream( connection.getOutputStream() );
-			ObjectInputStream in = new ObjectInputStream( connection.getInputStream() );
-			String message = "Connection Successful";
-			out.writeObject(message);
-			message = ( String ) in.readObject();
-			System.out.println ("Data from Client: " + message);
-			connection.close();
+			server = new ServerSocket(5000,10);
+			System.out.println("Server is listening on port 5000.");
 		}
 		catch (Exception ex)
+		{
+			System.err.println(ex);
+		}
+		
+		while (true)
+		{
+			try
+			{
+				System.out.println("Waiting for Client to Connect.");
+				Socket client = server.accept();
+				Thread r = new ReadingThread(client);
+				Thread w = new WritingThread(client);
+				w.start();
+				r.start();
+			}
+			catch (Exception ex)
+			{
+				System.err.println(ex);
+			}
+		}
+	}
+}
+
+class ReadingThread extends Thread
+{
+	private Socket connection;
+	
+	public ReadingThread(Socket con)
+	{
+		connection = con;
+	}
+	
+	public void run()
+	{
+		try
+		{
+			ObjectInputStream in = new ObjectInputStream (connection.getInputStream());
+			String message;
+			
+			while (true)
+			{
+				message = (String)in.readObject();
+				System.out.println(message);
+			}
+		}
+		catch(Exception ex)
+		{
+			System.err.println(ex);
+		}
+	}
+}
+
+class WritingThread extends Thread
+{
+	private Socket connection;
+	
+	public WritingThread(Socket con)
+	{
+		connection = con;
+	}
+	
+	public void run()
+	{
+		try
+		{
+			ObjectOutputStream out = new ObjectOutputStream(connection.getOutputStream());
+			String message;
+			Scanner sc = new Scanner(System.in);
+			
+			while(true)
+			{
+				message = sc.nextLine();
+				out.writeObject("Server: " + message);
+			}
+		}
+		catch(Exception ex)
 		{
 			System.err.println(ex);
 		}
